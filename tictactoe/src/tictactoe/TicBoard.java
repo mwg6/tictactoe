f92ef47dd7790d01c70569f1a03c4a7381da87ba
Max <maximgoodman@gmail.com>
2018-06-25 20:37:48 -0400

    Making this commit work

---
+++ tictactoe/src/tictactoe/TicBoard.java
@@ -0,0 +1,11 @@
+package tictactoe;
+
+public class TicBoard {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello World");
+//I've made a minor change now
+	}
+
+}
